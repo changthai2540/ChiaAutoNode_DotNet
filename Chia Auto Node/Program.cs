c992c37a79e4b197ead3696e9df6ab493dd808f8
unknown <pongpit.chang@gmail.com>
2021-05-21 22:05:42 +0700

    update check status node, fix cpu hight, can use every version chia.

--- Chia Auto Node/Program.cs
+++ Chia Auto Node/Program.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -41,34 +42,83 @@ namespace Chia_Auto_Node
             }
         }
 
+        public static bool PingHost(string hostUri, int portNumber)
+        {
+            try
+            {
+                TcpClient tcpClient = new TcpClient();
+                if (tcpClient.ConnectAsync(hostUri, portNumber).Wait(TimeSpan.FromMilliseconds(400)))
+                    return true;
+                else
+                    return false;
+            }
+            catch (SocketException)
+            {
+                return false;
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.Title = "iDisk Thailand - Auto Add Full Node";
-            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("------------------------------------------------------------------------------");
             Console.WriteLine("iDisk Thailand - Auto Add Full Node");
-            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Fanpage - https://www.facebook.com/IDISKThailand");
+            Console.WriteLine("------------------------------------------------------------------------------");
 
-            string chia_exec_path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\chia-blockchain\\app-1.1.5\\resources\\app.asar.unpacked\\daemon";
-            
-            while(true)
+            var FileExecute = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData).ToString();
+            if (Directory.Exists(FileExecute + "\\chia-blockchain"))
             {
-                string current_time = DateTime.Now.ToString("h:mm:ss tt");
-                Console.WriteLine("[" + current_time + "][System] Fetch new node from iDisk");
-                string RespNode = Get("https://chia-thailand.com/nodes/api.php?t=" + GetTimestamp(new DateTime()));
-                dynamic JsonResp = JsonConvert.DeserializeObject(RespNode);
-                foreach(dynamic Data in JsonResp)
+                string[] DirSeacrhAppChia = Directory.GetDirectories(FileExecute + "\\chia-blockchain", "app*", SearchOption.TopDirectoryOnly);
+                if (DirSeacrhAppChia.Length != 1)
                 {
-                    Console.WriteLine("[" + current_time + "][System] Try to add node - " + Data.IP);
-
-                    Process p = new Process();
-                    p.StartInfo = new ProcessStartInfo("chia.exe");
-                    p.StartInfo.WorkingDirectory = chia_exec_path;
-                    p.StartInfo.Arguments = "show -a " + Data.IP;
-                    p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-                    p.Start();
+                    Console.WriteLine("[" + DateTime.Now.ToString("h:mm:ss tt") + "][System]Please re-install Chia-Blockchian because program broken!");
                 }
+                else
+                {
+                    string AppChia = "";
+                    foreach (string DirAppChia in DirSeacrhAppChia)
+                    {
+                        AppChia = DirAppChia;
+                        break;
+                    }
+                    AppChia = AppChia + "\\resources\\app.asar.unpacked\\daemon";
 
-                Thread.Sleep(30000);
+                    while (true)
+                    {
+                        string current_time = DateTime.Now.ToString("h:mm:ss tt");
+                        Console.WriteLine("[" + current_time + "][System] Fetch new node from iDisk Thailand");
+                        string RespNode = Get("https://chia-thailand.com/nodes/api.php?t=" + GetTimestamp(new DateTime()));
+                        dynamic JsonResp = JsonConvert.DeserializeObject(RespNode);
+                        foreach (dynamic Data in JsonResp)
+                        {
+                            Console.WriteLine("[" + current_time + "][System] Node - " + Data.IP + ", pending");
+
+                            string[] SplitIPData = ((string)Data.IP).Split(':');
+                            if (PingHost(SplitIPData[0], Int32.Parse(SplitIPData[1])))
+                            {
+                                Console.WriteLine("[" + current_time + "][System] Node - " + Data.IP + ", connecting");
+
+                                Process p = new Process();
+                                p.StartInfo = new ProcessStartInfo("chia.exe");
+                                p.StartInfo.WorkingDirectory = AppChia;
+                                p.StartInfo.Arguments = "show -a " + Data.IP;
+                                p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                                p.Start();
+                            }
+                            else
+                            {
+                                Console.WriteLine("[" + current_time + "][System] Node - " + Data.IP + ", dead node");
+                            }
+                            Thread.Sleep(2000);
+                        }
+                        Thread.Sleep(30000);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("[" + DateTime.Now.ToString("h:mm:ss tt") + "][System]Please install Chia-Blockchian on you computer");
             }
         }
     }
